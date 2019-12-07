19e76d754894d5ce095d08589fce21343fb81c34
hari195 <harikrishnanr195@gmail.com>
2019-12-07 22:41:50 +0100

    Added css

--- webapp/webapp.py
+++ webapp/webapp.py
@@ -1,38 +1,28 @@
 from flask import Flask, url_for, render_template, request
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
 from config import Config
 import sys
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
-class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    githubpat = PasswordField('Github PAT', validators=[DataRequired()])
-    remember_me = BooleanField('Remember Me')
-    submit = SubmitField('Sign In')
-
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'you-will-never-guess'
 
-
+# Route for handling the login page logic
 @app.route('/')
-@app.route('/login', methods = ['GET', 'POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    form = LoginForm()
+    error = None
     if request.method == 'POST':
-        if form.validate() == False:
-            return render_template('login.html', form = form)
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            return 'Invalid Credentials. Please try again.'
         else:
-            print(form.username)
             return render_template('base.html')
     elif request.method == 'GET':
-            return render_template('login.html', form = form)
-    print(form.username)
+        return render_template('login.html', error=error)
 
+    
 
 #@app.route('/user/<username>')
 #def profile(username):
