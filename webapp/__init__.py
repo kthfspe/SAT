f204ba6b1a690affb440e3dcea1abaff5f39a34e
hari195 <harikrishnanr195@gmail.com>
2019-12-07 20:07:56 +0100

    Enabled autodoc

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,1 +1,8 @@
 import webapp
+
+def hello():
+    """ inits Spamfilter with training data
+        
+    :param training_dir: path of training directory with subdirectories
+     '/ham' and '/spam'
+    """ 
