4257d11991312af7e2b07650476c5c9cce4fdb32
hari195 <harikrishnanr195@gmail.com>
2019-12-07 20:10:04 +0100

    Removed __init__ everywhere

--- webapp/__init__.py
+++
@@ -1,8 +0,0 @@
-import webapp
-
-def hello():
-    """ inits Spamfilter with training data
-        
-    :param training_dir: path of training directory with subdirectories
-     '/ham' and '/spam'
-    """ 
