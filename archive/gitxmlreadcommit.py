e0f5b2742eed3eb9f50899964f9656aa60b8b41a
hari195 <harikrishnanr195@gmail.com>
2019-12-18 22:31:32 +0100

    gitignore test

--- archive/gitxmlreadcommit.py
+++ archive/gitxmlreadcommit.py
@@ -2,7 +2,7 @@ from github import Github
 import base64
 
 # or using an access token
-g = Github("blablablablabla")
+g = Github("blablablabla")
 
 # for repo in g.get_user().get_repos():
     # print(repo.name)
