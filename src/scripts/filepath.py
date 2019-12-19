0cb0f54b6f3da314e6806e1b83161bb47482055b
hari195 <harikrishnanr195@gmail.com>
2019-12-19 20:21:24 +0100

    Corrected filepaths

--- src/scripts/filepath.py
+++ src/scripts/filepath.py
@@ -2,19 +2,19 @@ debug = False
 
 if debug == False:
     defaultrepo = "kthfspe/SA"
-    defaultLVphy = "/LV_architecture/LV_physical_architecture"
-    defaultLVfun = "/LV_architecture/LV_functional_architecture"
-    defaultHVphy = "/HV_architecture/HV_physical_architecture"
-    defaultHVfun = "/HV_architecture/HV_functional_architecture"
-    defaultDVphy = "/DV_architecture/DV_physical_architecture"
-    defaultDVfun = "/DV_architecture/DV_functional_architecture"
+    defaultLVphy = "LV_architecture/LV_physical_architecture"
+    defaultLVfun = "LV_architecture/LV_functional_architecture"
+    defaultHVphy = "HV_architecture/HV_physical_architecture"
+    defaultHVfun = "HV_architecture/HV_functional_architecture"
+    defaultDVphy = "DV_architecture/DV_physical_architecture"
+    defaultDVfun = "DV_architecture/DV_functional_architecture"
 
 else:
     defaultrepo = "kthfspe/SA"
-    defaultLVphy = "/example/LV_architecture/LV_physical_architecture"
-    defaultLVfun = "/example/LV_architecture/LV_functional_architecture"
-    defaultHVphy = "/example/HV_architecture/HV_physical_architecture"
-    defaultHVfun = "/example/HV_architecture/HV_functional_architecture"
-    defaultDVphy = "/example/DV_architecture/DV_physical_architecture"
-    defaultDVfun = "/example/DV_architecture/DV_functional_architecture"
+    defaultLVphy = "example/LV_architecture/LV_physical_architecture"
+    defaultLVfun = "example/LV_architecture/LV_functional_architecture"
+    defaultHVphy = "example/HV_architecture/HV_physical_architecture"
+    defaultHVfun = "example/HV_architecture/HV_functional_architecture"
+    defaultDVphy = "example/DV_architecture/DV_physical_architecture"
+    defaultDVfun = "example/DV_architecture/DV_functional_architecture"
 
