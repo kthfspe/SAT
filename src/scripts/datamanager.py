d4a180bd3f3abd4f34df025681530075e6240dba
hari195 <harikrishnanr195@gmail.com>
2019-12-24 02:41:56 +0100

    Creates separate json files for all data

--- src/scripts/datamanager.py
+++ src/scripts/datamanager.py
@@ -13,9 +13,12 @@ class DataManager:
     merged_functional = []
     data_physical = []
     data_functional = []
-    raw_db = TinyDB(filepath.db_path+"/raw_db.json")
-    merged_db = TinyDB(filepath.db_path+"/merged_db.json")
-    db = TinyDB(filepath.db_path+"/db.json")
+    raw_pdb = TinyDB(filepath.db_path+"/raw_pdb.json")
+    merged_pdb = TinyDB(filepath.db_path+"/merged_pdb.json")
+    pdb = TinyDB(filepath.db_path+"/pdb.json")
+    raw_fdb = TinyDB(filepath.db_path+"/raw_fdb.json")
+    merged_fdb = TinyDB(filepath.db_path+"/merged_fdb.json")
+    fdb = TinyDB(filepath.db_path+"/fdb.json")
 
     def __init__(self):
         pass
@@ -23,4 +26,5 @@ class DataManager:
     def loadrawdb(self, rp, rf):
         self.raw_physical = rp
         self.raw_functional = rf
+        
         #Load to json file locally and return path and status
