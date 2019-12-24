79b9b72a57a9a52a80abd01e6fd6f98c268b1d51
hari195 <harikrishnanr195@gmail.com>
2019-12-24 16:18:27 +0100

    Checks forinvalid blocktypes in each file

--- src/frame.py
+++ src/frame.py
@@ -5,6 +5,7 @@ import xml.etree.ElementTree as ET
 
 raw_physical = []
 raw_functional = []
+errormessage = []
 root = ET.parse('LV_functional_architecture').getroot()
 for child in root.findall('diagram/mxGraphModel/root/object'):
     a = dict(child.attrib)
@@ -20,19 +21,23 @@ with open("raw_pdb.json", 'w') as fout:
 
 with open("raw_fdb.json", 'w') as fout2:
     json.dump(raw_functional, fout2)
-    
+
+#Physical Architecture Basic Validity Checking
 with open('raw_pdb.json', 'r') as fp:
     data = json.load(fp)
 
 for item in data:
     if item != None:
-        print(item["BlockType"])
-        pass
+        if item["BlockType"] not in ["DIG","ANA","SENS","ECU","CAN","NCU","BAT","FCON","MCON","ACT","HMI"]:
+            errormessage.append("Invalid Block in physical architecture with BlockType " + item["BlockType"] + " and ID " + item["id"]  )
 
 with open('raw_fdb.json', 'r') as fp:
     data2 = json.load(fp)
 
 for item in data2:
     if item != None:
-        # print(item['BlockType'])
-        pass
+        if item["BlockType"] not in ["ENVIN","ENVOUT","FE"]:
+            errormessage.append("Invalid Block in functional architecture with BlockType " + item["BlockType"] + " and ID " + item["id"]  )
+
+
+print(errormessage)
