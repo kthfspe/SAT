1c80212b233cc6c6d310805834b68aba35111f06
hari195 <harikrishnanr195@gmail.com>
2020-01-29 21:18:52 +0100

    Removes blocks from global ignore list

--- src/scripts/blocklist.py
+++ src/scripts/blocklist.py
@@ -1,3 +1,3 @@
 physical_blocktypes = ["DIG","ANA","SENS","OTSC","ECU","CAN","NCU","BAT","FCON","MCON","ACT","HMI","PCU"]
 functional_blocktypes = ["ENVIN","ENVOUT","FE","FS"]
-ignore_blocktype = ["IGNORE", "FRAME"]
+ignore_blocktype = [ "FRAME", "IGNORE"]
--- src/scripts/datamanager.py
+++ src/scripts/datamanager.py
@@ -21,6 +21,9 @@ class DataManager:
         self.raw_physical = rp
         self.raw_functional = rf
 
+        # Remove ignored blocks
+        self.removeignoredblocks()
+
         # check blockvalidity
         self.checkblockvalidity()
 
@@ -36,20 +39,30 @@ class DataManager:
         # self.status == 0 if all ok, else == 1
         # return self.error, self.warning
 
-    def checkblockvalidity(self):
+def removeignoredblocks(self):  
+        # Remove blocks to be ignored
+        tempf = []
+        tempp = []
+        for item in self.raw_functional:
+            if item['BlockType'] not in blocklist.ignore_blocktype:
+                tempf.append(item)
+        self.raw_functional = tempf
+        for item in self.raw_physical:
+            if item['BlockType'] not in blocklist.ignore_blocktype:
+                tempp.append(item)
+        self.raw_physical = tempp
 
+def checkblockvalidity(self):
         # Physical Architecture - Block Validity Checking
         for item in self.raw_physical:
-            if item["BlockType"] not in blocklist.ignore_blocktype and \
-                item["BlockType"] not in blocklist.physical_blocktypes:
+            if item["BlockType"] not in blocklist.physical_blocktypes:
                 self.warning.append("Invalid Block in Physical Architecture with BlockType " + item["BlockType"] + \
                    ", Name " + item['Name'] + " and ID " + item["id"]  )
             else:
                 self.corrected_raw_physical.append(item)
         # Functional Architecture - Block Validity Checking
         for item in self.raw_functional:
-            if item["BlockType"] not in blocklist.ignore_blocktype and \
-                item["BlockType"] not in blocklist.functional_blocktypes:
+            if item["BlockType"] not in blocklist.functional_blocktypes:
                 self.warning.append("Invalid Block in Functional Architecture with BlockType " + item["BlockType"] + \
                         ", Name " + item['Name'] + " and ID " + item["id"]  )
             else:
@@ -66,13 +79,13 @@ class DataManager:
         #for item in raw_complete:
         #    namelist.append(item['Name'])
 
-        print(self.warning)
+        # print(self.warning)
         for item in raw_complete:
             # Global Rules(GR) - Rules applying to all blocks
 
             # GR1: Name exists and is non-empty
             if 'Name' not in item or item['Name']=='':
-                print(item)
+                print(item['BlockType'])
 
             # GR2: Check parent validity
             #if 'Parent' in item:
