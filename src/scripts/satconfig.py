9f0c3e463b4469f4c4e9f19b48421fd818f86e0b
hari195 <harikrishnanr195@gmail.com>
2020-02-15 21:30:45 +0100

    Global config file satconfig.py

--- src/scripts/satconfig.py
+++ src/scripts/satconfig.py
@@ -1,3 +1,6 @@
+# Main configuration file for SAT app
+# This can be used to create a settings page in the app so some of these can be changed via the web interface.
+
 debug = False
 gitaccount = "kthfspe"
 gitrepo = "SA"
