c28a80e2cf94628565d1fedcd96a2c175a36d488
hari195 <harikrishnanr195@gmail.com>
2020-02-15 22:50:57 +0100

    Cleaned up code

--- src/scripts/satconfig.py
+++ src/scripts/satconfig.py
@@ -1,7 +1,15 @@
 # Main configuration file for SAT app
 # This can be used to create a settings page in the app so some of these can be changed via the web interface.
 
+# App status
 debug = False
+
+# GIT related
 gitaccount = "kthfspe"
 gitrepo = "SA"
 gitpath = gitaccount + "/" + gitrepo
+
+# File path related
+
+
+# Blocklist related
